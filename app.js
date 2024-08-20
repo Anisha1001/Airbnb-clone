6cba2143426a4f5dacdc4c539e8c813815e5eb6b
Anisha Chowdhury <anishachowdhury34@gmail.com>
2024-08-20 19:40:16 +0530

    changd

--- app.js
+++ app.js
@@ -6,26 +6,24 @@ if(process.env.NODE_ENV !="production"){
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const{isLoggedIn,isOwner,isreviewAuthor}=require("./middleware.js");
 const path = require("path");
-const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
-const session=require("express-session")
-const MongoStore = require('connect-mongo');
-
-
+const methodOverride = require("method-override");
 const flash=require("connect-flash");
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const passport=require("passport");
 const localStrategy=require("passport-local");
+const ExpressError = require("./utils/ExpressError.js");
 const User=require("./models/user.js");
+
+const{isLoggedIn,isOwner,isreviewAuthor}=require("./middleware.js");
+const wrapAsync = require("./utils/wrapAsync.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 const { is } = require("express/lib/request.js");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl=process.env.ATLASDB_URL;
+//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const{saveRedirectUrl}=require("./middleware.js");
 const listingController=require("./controller/listing.js");
 const reviewController=require("./controller/review.js");
@@ -38,18 +36,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(flash());
+const mongoUrl = process.env.ATLASDB_URL;
+
 const store=MongoStore.create({
-  mongoUrl:dbUrl,
-  crypto:{
-    secret:process.env.SECRET,
-  },
-  touchAfter:24*3600,
-});
-store.on("error",()=>{
-  console.log("ERROR IN MONO SESSION STORE",err)
+    mongoUrl:mongoUrl,
+    crypto:{
+      secret:process.env.SECRET
+    },
 })
+store.on("error",()=>{
+  console.log("Error in mongo session store",err);
+});
 const sessionOptions={
-  store,
+  store:store,
   secret:process.env.SECRET,
   resave:false,
   saveUninitialized:true,
@@ -59,9 +59,10 @@ const sessionOptions={
        httpOnly:true,
   }
 };
+
 app.use(session(sessionOptions));
 
-app.use(flash());
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
@@ -127,7 +128,7 @@ main()
   });
 
   async function main() {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(mongoUrl);
   }
   
 
